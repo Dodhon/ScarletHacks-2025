767b58c35d36f776f7ee7f1a02016df8380c0374
Thupten Wangpo <110739214+Dodhon@users.noreply.github.com>
2025-04-05 17:12:17 -0500

    Pinged your deployment. You successfully connected to MongoDB!

--- vectors.py
+++ vectors.py
@@ -1,8 +1,15 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os 
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://jchmura:<db_password>@hackathon-2025.thsauke.mongodb.net/?appName=hackathon-2025"
+load_dotenv()
+
+password = os.environ.get("DB_PASSWORD") 
+if not password:
+    raise ValueError("MongoDB password not found in environment variable DB_PASSWORD")
+
+uri = os.environ.get("DB_CONNECTION_STRING")
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
